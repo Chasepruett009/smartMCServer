29c7dd3192e56d685efe3aec786208a9353e9beb
Chasepruett009 <chasedpruett009@gmail.com>
2021-11-23 04:00:14 -0500

    Test Commit of Unfinished Framework and Test UI

---
+++ smartMCServer/MVVM/View/BackupsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace smartMCServer.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for BackupsView.xaml
+    /// </summary>
+    public partial class BackupsView : UserControl
+    {
+        public BackupsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ smartMCServer/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,108 @@
+﻿using smartMCServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartMCServer.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ConsoleViewCommand { get; set; }
+        public RelayCommand PlayersViewCommand { get; set; }
+        public RelayCommand WorldsViewCommand { get; set; }
+        public RelayCommand BackupsViewCommand { get; set; }
+        public RelayCommand TaskSchedulerViewCommand { get; set; }
+        public RelayCommand ServerConfigViewCommand { get; set; }
+        public RelayCommand SMCSSettingsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public ConsoleViewModel ConsoleVM { get; set; }
+        public PlayersViewModel PlayersVM { get; set; }
+        public WorldsViewModel WorldsVM { get; set; }
+        public BackupsViewModel BackupsVM { get; set; }
+        public TaskSchedulerViewModel TaskSchedulerVM { get; set; }
+        public ServerConfigViewModel ServerConfigVM { get; set; }
+        public SMCSSettingsViewModel SMCSSettingsVM { get; set; }
+
+        private object  _currentView;
+
+        public object  CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+
+            ConsoleVM = new ConsoleViewModel();
+
+            PlayersVM = new PlayersViewModel();
+
+            WorldsVM = new WorldsViewModel();
+
+            BackupsVM = new BackupsViewModel();
+
+            TaskSchedulerVM = new TaskSchedulerViewModel();
+
+            ServerConfigVM = new ServerConfigViewModel();
+
+            SMCSSettingsVM = new SMCSSettingsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVM;
+            });
+
+            ConsoleViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ConsoleVM;
+            });
+
+            PlayersViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PlayersVM;
+            });
+
+            WorldsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = WorldsVM;
+            });
+
+            BackupsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = BackupsVM;
+            });
+
+            TaskSchedulerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TaskSchedulerVM;
+            });
+
+            ServerConfigViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ServerConfigVM;
+            });
+
+            SMCSSettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SMCSSettingsVM;
+            });
+        }
+
+
+
+    }
+    }
